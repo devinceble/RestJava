2aa02a954400a64d6f0005a2c84cb74eed92ace5
devinceble <devinceble@gmail.com>
2013-07-14 10:40:11 +0800

    Initialize RestJava

---
+++ src/io/restjava/core/RestJava.java
@@ -0,0 +1,17 @@
+package io.restjava.core;
+
+
+
+/**
+ * RestJava Main Class
+ * @author devinceble Leivince John D. Marte (devinceble@gmail.com)
+ * @version 0.0.1-beta
+ * 
+ */
+public class RestJava {
+    
+    public void init(){
+        System.out.println("Initialize RestJava");
+    }
+    
+}
